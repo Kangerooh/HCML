64d37134ef70034ac81c1030dfc1295021eb6173
theresaschludi <theresa.schludi@gmx.de>
2026-06-08 08:02:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+from folktables import ACSDataSource, ACSEmployment
+
+data_source = ACSDataSource(survey_year='2018', horizon='1-Year', survey='person')
+acs_data = data_source.get_data(states=["AL"], download=True)
+features, label, group = ACSEmployment.df_to_numpy(acs_data)
